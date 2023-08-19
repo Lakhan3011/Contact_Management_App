9a6e9da8ebcc505f6aeb8b34ba7fce80a609f985
Lakhan lal <lakhan191136@dei.ac.in>
2023-08-19 16:57:04 +0530

    complete

---
+++ src/Components/ContactForm.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addContact } from "../Redux/action";
+function ContactForm() {
+  const dispatch = useDispatch();
+
+  const [form, setForm] = useState({
+    first_name: "",
+    last_name: "",
+    status: "active",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  function handleSave() {
+    dispatch(addContact(form));
+  }
+
+  return (
+    <div className="w-1/2 mx-auto my-10 pt-16">
+      <h2 className="text-2xl font-bold mb-10">Create Contact</h2>
+      <div className="mb-6 flex">
+        <label className="block font-bold mr-2 py-1" htmlFor="first-name">
+          FirstName
+        </label>
+        <input
+          className="w-full border border-gray-400 p-2 rounded-md"
+          id="first-name"
+          type="text"
+          name="first_name"
+          value={form.first_name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-6  flex">
+        <label className="block  font-bold mr-2 py-1" htmlFor="last-name">
+          LastName
+        </label>
+        <input
+          className="w-full border border-gray-400 p-2 rounded-md"
+          id="last-name"
+          type="text"
+          name="last_name"
+          value={form.last_name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-6 flex">
+        <label className="block font-bold mr-9 py-1" htmlFor="status">
+          Status
+        </label>
+        <select
+          className="w-full border border-gray-400 p-2 rounded-md"
+          id="status"
+          name="status"
+          value={form.status}
+          onChange={handleChange}
+        >
+          <option value={"active"}>Active</option>
+          <option value={"inactive"}>Inactive</option>
+        </select>
+      </div>
+      <button onClick={handleSave}>
+        <a
+          href="#_"
+          class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-gray-500 rounded-xl group"
+        >
+          <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-gray-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+            <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+          </span>
+          <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-gray-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-white font-bold transition-colors duration-200 ease-in-out group-hover:text-white">
+            Save Contact
+          </span>
+        </a>
+      </button>
+    </div>
+  );
+}
+
+export default ContactForm;
---
+++ src/Components/WorldMap.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import markerIcon from "../utils/mark.png";
+const WorldMap = (countries) => {
+  const { countriesData } = countries;
+  const customMarker = L.icon({
+    iconUrl: markerIcon,
+    iconSize: [20, 25],
+    iconAnchor: [15, 30],
+  });
+
+  return (
+    <div>
+      {countriesData?.map((country) => (
+        <Marker
+          icon={customMarker}
+          key={country.countryInfo._id}
+          position={[country.countryInfo.lat, country.countryInfo.long]}
+        >
+          <Popup>
+            <div>
+              <h2>{country.country}</h2>
+              <p>
+                Active Cases: {country.active} <br />
+                Recovered Cases: {country.recovered} <br />
+                Deaths: {country.deaths}
+              </p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </div>
+  );
+};
+
+export default WorldMap;
